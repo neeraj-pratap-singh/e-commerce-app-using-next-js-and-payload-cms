15fdad0285901c79157f588f31dbdda6c5cc87a6
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2024-01-21 01:20:14 +0530

    implemented categories

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -20,9 +20,9 @@ import { generateMeta } from '../../_utilities/generateMeta'
 // If you are not using Payload Cloud then this line can be removed, see `../../../README.md#cache`
 export const dynamic = 'force-dynamic'
 
-// import Categories from '../../_components/Categories'
-// import Promotion from '../../_components/Promotion'
+import Categories from '../../_components/Categories'
 
+// import Promotion from '../../_components/Promotion'
 import classes from './index.module.scss'
 
 export default async function Page({ params: { slug = 'home' } }) {
@@ -65,10 +65,10 @@ export default async function Page({ params: { slug = 'home' } }) {
         <section>
           <Hero {...hero} />
 
-          {/* <Gutter className={classes.home}>
+          <Gutter className={classes.home}>
             <Categories categories={categories} />
-            <Promotion />
-          </Gutter> */}
+            {/* <Promotion /> */}
+          </Gutter>
         </section>
       ) : (
         <>
