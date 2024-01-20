f65664d69aae92c4a6eaac79a3a377c659b3b9ce
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2024-01-21 01:35:34 +0530

    added Promotions component

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -21,8 +21,8 @@ import { generateMeta } from '../../_utilities/generateMeta'
 export const dynamic = 'force-dynamic'
 
 import Categories from '../../_components/Categories'
+import Promotion from '../../_components/Promotion'
 
-// import Promotion from '../../_components/Promotion'
 import classes from './index.module.scss'
 
 export default async function Page({ params: { slug = 'home' } }) {
@@ -67,7 +67,7 @@ export default async function Page({ params: { slug = 'home' } }) {
 
           <Gutter className={classes.home}>
             <Categories categories={categories} />
-            {/* <Promotion /> */}
+            <Promotion />
           </Gutter>
         </section>
       ) : (
